e87b18eeddf8e524a8df3f3eb6c70d4fe775ca58
steelem0 <marcysteele83@gmail.com>
2025-07-06 13:14:38 -0400

    game play updates

--- components/GameLog.vue
+++ components/GameLog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="gamelog-container py-10">
+  <div ref="logContainer" class="gamelog-container py-10">
     <div v-for="(entry, index) in entries" :key="index" class="entry">
       <p :class="entry.from">{{ entry.text }}</p>
     </div>
@@ -7,12 +7,38 @@
 </template>
 
 <script setup>
-defineProps({
+import { ref, watch, onMounted, nextTick } from 'vue'
+
+const props = defineProps({
   entries: {
     type: Array,
     required: true
   }
 })
+
+const logContainer = ref(null)
+
+function scrollToBottom() {
+  if (logContainer.value) {
+    logContainer.value.scrollTop = logContainer.value.scrollHeight
+  }
+}
+
+// Scroll on mount
+onMounted(() => {
+  scrollToBottom()
+})
+
+watch(
+  () => props?.entries?.length,
+  async () => {
+    await nextTick()
+    if (logContainer.value) {
+      logContainer.value.scrollTop = logContainer.value.scrollHeight
+    }
+  }
+)
+
 </script>
 
 <style scoped>
